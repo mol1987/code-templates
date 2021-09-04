d28b221ca90a8a843b179717cffe0fb4a1db401b
sadqw fwe <fmolen@gmail.com>
2021-09-04 12:51:50 +0200

    Fix folder structure

--- template.bat
+++ template.bat
@@ -4,25 +4,26 @@ set dest=C:\Users\fmole\Documents\repos
 
 
 if [%1] == [] (
-echo #######################TEMPLATE##############################
-echo # desc: Copies a template code structure		    #
-echo #	     from a list of templates			    #
-echo #	     to any destination			            #
-echo # commands: 				                    #
-echo #  ^>template list		: Lists all templates 	    # 
-echo #  ^>template ^<template^> ^<name^> 	: Copies template folder    #
-echo #############################################################
+echo #######################TEMPLATE###################################
+echo # desc: Copies a template code structure		    	 #
+echo #	     from a list of templates			    	 #
+echo #	     to any destination			            	 #
+echo # commands: 				                    	 #
+echo #  ^>template list	^<folder^>	: Lists all templates 	 # 
+echo #  ^>template ^<folder\template^> ^<name^> 	: Copies template folder #
+echo ##################################################################
 echo source folder: 		%source%
 echo destination folder:	%dest%                    
 goto done
 )
  
 if %1 == list (
-dir /ad /D %source%
+dir /ad /D %source%\%2
 goto done
 )
+  
 xcopy /I /E %source%\%1 %dest%\%2
 
-echo %dest%\%2
+echo Created template: %dest%\%2
 
 :done
